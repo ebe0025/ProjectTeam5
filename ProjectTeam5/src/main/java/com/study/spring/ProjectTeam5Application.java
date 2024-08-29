e1279ef06575f998e0e428ba98180e59e0a6d61b
ebe0025 <tjdgns3728@naver.com>
2024-08-29 12:39:44 +0900

    111

---
+++ ProjectTeam5/src/main/java/com/study/spring/ProjectTeam5Application.java
@@ -0,0 +1,13 @@
+package com.study.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTeam5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTeam5Application.class, args);
+	}
+
+}
---
+++ ProjectTeam5/src/test/java/com/study/spring/ProjectTeam5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTeam5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
