74c8a1914c946a6a21ed3e5790c2067dcc09abea
ebe0025 <tjdgns3728@naver.com>
2024-08-31 13:40:21 +0900

    다시 레파토리 수정
    다시 레파토리 수정

--- ProjectTeam5/src/main/java/com/study/spring/repository/FriendsRepository.java
+++ ProjectTeam5/src/main/java/com/study/spring/repository/FriendsRepository.java
@@ -3,7 +3,9 @@ package com.study.spring.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.study.spring.domain.Friends;
+
 @Repository
-public interface FriendsRepository extends JpaRepository<FriendsRepository, Long> {
+public interface FriendsRepository extends JpaRepository<Friends, Long> {
 
 }
