6814ec6f387d00df9f41d25bc3b7dbf7be840b4d
kurasama <was0502@naver.com>
2024-09-02 16:52:11 +0900

    k

---
+++ ProjectTeam5/avatar-generator/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import AvatarGenerator from './components/AvatarGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <AvatarGenerator />
+    </div>
+  );
+}
+
+export default App;
