70811540372a1a78bed03395ed719e9eca8affb7
ebe0025 <tjdgns3728@naver.com>
2024-09-03 12:07:43 +0900

    병합 및 수정
    병합 및 수정

--- ProjectTeam5/src/main/java/com/study/spring/controller/DiaryController.java
+++ ProjectTeam5/src/main/java/com/study/spring/controller/DiaryController.java
@@ -1,7 +1,6 @@
 package com.study.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.study.spring.service.DiaryService;
--- ProjectTeam5/src/main/java/com/study/spring/controller/MemberController.java
+++ ProjectTeam5/src/main/java/com/study/spring/controller/MemberController.java
@@ -1,13 +1,63 @@
 package com.study.spring.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.study.spring.domain.Member;
+import com.study.spring.repository.MemberRepository;
 import com.study.spring.service.MemberService;
+import com.study.spring.util.JwtUtil;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3003") // CORS 설정
+@RequestMapping("/api/auth")
 public class MemberController {
+	
+	
 	@Autowired
 	private MemberService memberService;
+	
+	@Autowired
+    private MemberRepository memberRepository;
+	
+   @PostMapping("/signup")
+    public String signup(@RequestBody Member member) {
+	   
+        memberService.insertMember(member); // 사용자 저장
+        
+        return "User registered successfully"; // 등록 성공 메시지
+    }
+
+    @PostMapping("/login")
+    public List<Object> login(@RequestBody Member loginRequest) {
+    	
+        Optional<Member> member = memberRepository.findById(loginRequest.getMemId()); // 사용자 조회
+        System.out.println("loginRequest :" + loginRequest);
+        
+        if (member.isPresent() && member.get().getPass().equals(loginRequest.getPass())) {
+            List<Object> list = new ArrayList<>();  // ArrayList로 초기화
+            
+            // JwtUtil을 사용하여 JWT 생성
+            String token = JwtUtil.generateToken(member.get().getMemId());
+            System.out.println("token :" + token);
+            
+            // 리스트에 토큰과 아이디를 추가
+            list.add(token);
+            list.add(member.get().getMemId());
+            
+            return list; // JWT와 아이디가 담긴 리스트 반환
+        }
+        
+        return Collections.singletonList("Invalid credentials"); // 잘못된 인증 정보
+    }	
+
 }
