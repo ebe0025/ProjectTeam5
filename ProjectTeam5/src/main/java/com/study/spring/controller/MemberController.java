cbbeb1484cc0a8b4c5c6e2feb892dda1eed1c388
ebe0025 <tjdgns3728@naver.com>
2024-09-03 11:33:36 +0900

    컨트롤러 수정
    컨트롤러 수정

--- ProjectTeam5/src/main/java/com/study/spring/controller/MemberController.java
+++ ProjectTeam5/src/main/java/com/study/spring/controller/MemberController.java
@@ -2,11 +2,25 @@ package com.study.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.study.spring.domain.Member;
 import com.study.spring.service.MemberService;
 
-@Controller
+@RestController
 public class MemberController {
 	@Autowired
 	private MemberService memberService;
+	
+	@PostMapping("member/register")
+	public String insertMember(@RequestBody Member member) {
+		
+		memberService.insertMember(member);
+		return "success";
+	}
+	
 }
