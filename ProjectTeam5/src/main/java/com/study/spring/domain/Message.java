9ff978a586605e89e7bf1f0b21b49664a23c57ee
ebe0025 <tjdgns3728@naver.com>
2024-09-04 15:25:49 +0900

    엔티티 수정
    엔티티 수정

--- ProjectTeam5/src/main/java/com/study/spring/domain/Message.java
+++ ProjectTeam5/src/main/java/com/study/spring/domain/Message.java
@@ -1,11 +1,23 @@
 package com.study.spring.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Data;
 
+@Data
 @Entity
 public class Message {
+	@Id
+	@SequenceGenerator (
+			name="mSEQ",
+			sequenceName = "m_SQ",
+			allocationSize = 1
+			)	
+	@GeneratedValue(generator="mSEQ")
 	private Long mNum;
 	
 	@ManyToOne
@@ -14,4 +26,5 @@ public class Message {
 	
 	private String mContent;
 	private String createSysdate;
+	private boolean isReading;
 }
